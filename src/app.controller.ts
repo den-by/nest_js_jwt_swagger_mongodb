c2117a515e71e7047422228f434920e03470aa3f
denis <denis.da.by@gmail.com>
2021-06-27 19:49:54 +0300

    add swagger

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('me')
+  getMe(): string {
+    return 'me';
+  }
+
+  @Post('sinup')
+  postSignup(): string {
+    return 'sinup';
+  }
+
+  @Post('singin')
+  postSignin(): string {
+    return 'singin';
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('Accounts')
+    .setDescription('The Accounts API description')
+    .setVersion('1.0')
+    .addTag('accounts')
+    .addBearerAuth()
+    .build();
+
+  const accountDocument = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api/account', app, accountDocument);
+
   await app.listen(3000);
 }
 bootstrap();
